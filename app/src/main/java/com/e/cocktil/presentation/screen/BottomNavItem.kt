ebfc77b0b1b03143338a8f52e241244f7327566b
DESKTOP-7VMLLDQ\user <dhaval.lenovo@gmail.com>
2023-09-13 18:04:38 +0530

    Name changes

--- app/src/main/java/com/e/cocktil/presentation/screen/BottomNavItem.kt
+++ app/src/main/java/com/e/cocktil/presentation/screen/BottomNavItem.kt
@@ -4,7 +4,7 @@ import com.e.cocktil.R
 
 sealed class BottomNavItem(var title:String, var icon:Int, var screen_route:String){
 
-    object Home : BottomNavItem("Home", R.drawable.ic_home,"home")
+    object Home : BottomNavItem("Discover", R.drawable.ic_home,"home")
     object Search: BottomNavItem("Search",R.drawable.ic_search,"search")
     object Account: BottomNavItem("Account",R.drawable.ic_account,"account")
 }
